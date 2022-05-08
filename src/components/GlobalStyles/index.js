44a5e214d9600e3b4a19f41c60888c3a91ea6826
Kien Quan <hotrankienquan144@gmail.com>
2022-05-08 10:31:31 +0700

    cau hinh router and xay dung co che tai layout

--- src/components/GlobalStyles/index.js
+++ src/components/GlobalStyles/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
 import './GlobalStyles.scss';
 function GlobalStyles({ children }) {
-    return <div>GlobalStyles</div>;
+    return children;
 }
 
 export default GlobalStyles;
